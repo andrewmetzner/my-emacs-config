197ec9d705095ad609cda0d6b49ef38269b76da5
emacslispdude <luckless-wizard@riseup.net>
2026-03-12 23:09:37 -0400

    org

--- init.el
+++ init.el
@@ -8,3 +8,36 @@
 
   (when (file-exists-p config-org)
     (org-babel-load-file config-org)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(jabber-avatar-max-height 0)
+ '(jabber-avatar-max-width 0)
+ '(jabber-chat-buffer-show-avatar nil t)
+ '(jabber-vcard-avatars-retrieve nil t)
+ '(org-pomodoro-audio-player "paplay")
+ '(package-selected-packages nil)
+ '(pdf-annot-default-annotation-properties
+   '((t (label . "Andrew Metzner"))
+     (text (color . "#ff0000") (icon . "Note"))
+     (highlight (color . "deep sky blue"))
+     (underline (color . "blue")) (squiggly (color . "orange"))
+     (strike-out (color . "red"))))
+ '(pdf-view-midnight-colors '("#FFFFFF" . "#000000")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-current-nick-face ((t (:foreground "red" :weight bold))))
+ '(erc-input-face ((t (:foreground "red"))))
+ '(erc-my-nick-face ((t (:foreground "red" :weight bold))))
+ '(jabber-activity-personal-face ((t (:foreground "cyan" :weight bold))))
+ '(jabber-chat-prompt-local ((t (:foreground "cyan" :weight bold))))
+ '(jabber-rare-time-face ((t (:foreground "green" :underline t))))
+ '(jabber-roster-user-away ((t (:foreground "yellow" :slant italic :weight normal))))
+ '(jabber-roster-user-online ((t (:foreground "cyan" :slant normal :weight bold))))
+ '(jabber-title-large ((t (:inherit variable-pitch :weight bold :height 2.0 :width ultra-expanded))))
+ '(jabber-title-medium ((t (:inherit variable-pitch :weight bold :height 1.5 :width expanded)))))
